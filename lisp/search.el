a2891a8bf84192553542106cee4d6ec7c60e4381
leodcs <aleotory@gmail.com>
2020-07-23 11:42:13 -0300

    Fallback swiper-isearch posframe

--- lisp/search.el
+++ lisp/search.el
@@ -18,6 +18,8 @@
   (use-package counsel)
   (use-package ivy-posframe
     :config
-    (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-center))
-          ivy-posframe-parameters '((left-fringe . 8) (right-fringe . 8)))
+    (setq ivy-posframe-parameters '((left-fringe . 8) (right-fringe . 8))
+          ivy-posframe-display-functions-alist
+          '((swiper-isearch          . ivy-display-function-fallback)
+            (t . ivy-posframe-display-at-window-center)))
     (ivy-posframe-mode 1)))
