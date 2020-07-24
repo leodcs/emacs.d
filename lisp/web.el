c63abf98f043dc6d0acf78da624e996728b4598b
Leonardo Santos <aleotory@gmail.com>
2020-07-23 22:03:00 -0300

    Use package restclient

--- lisp/web.el
+++ lisp/web.el
@@ -7,8 +7,4 @@
     (setq web-mode-markup-indent-offset 2))
   (add-hook 'web-mode-hook  'leo/web-mode-hook))
 
-(use-package verb
-  :config
-  (use-package org
-  :mode ("\\.org\\'" . org-mode)
-  :config (define-key org-mode-map (kbd "C-c C-r") verb-command-map)))
+(use-package restclient)
