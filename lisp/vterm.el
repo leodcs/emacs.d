d704b816c0b41c796e261ab91a6ae0435961cbc9
Leonardo Santos <aleotory@gmail.com>
2020-07-25 00:34:34 -0300

    Wrap vterm on use-package function

--- lisp/vterm.el
+++ lisp/vterm.el
@@ -1,36 +1,36 @@
-(add-hook 'vterm-mode-hook 'leo/vterm-mode-enter)
-
 (use-package vterm
   :load-path  "~/.emacs.d/vendor/emacs-libvterm/"
   :config
-  (setq vterm-max-scrollback 20000000))
+  (setq vterm-max-scrollback 20000000)
 
-(defun leo/run-vterm-console ()
-  "Opens a new instance of vterm everytime it is called."
-  (interactive)
-  (vterm)
-  (evil-insert-state))
+  (add-hook 'vterm-mode-hook 'leo/vterm-mode-enter)
 
-(defun leo/projectile-run-vterm ()
-  "Opens one single instance of vterm inside current projectile project."
-  (interactive)
-  (split-window-vertically)
-  (other-window 1)
-  (evil-window-move-very-bottom)
-  (projectile-run-vterm)
-  (evil-insert-state))
+  (defun leo/run-vterm-console ()
+    "Opens a new instance of vterm everytime it is called."
+    (interactive)
+    (vterm)
+    (evil-insert-state))
 
-(defun leo/vterm-mode-enter ()
-  (interactive)
-  (setq-local evil-move-cursor-back nil)
-  (rvm-activate-corresponding-ruby))
+  (defun leo/projectile-run-vterm ()
+    "Opens one single instance of vterm inside current projectile project."
+    (interactive)
+    (split-window-vertically)
+    (other-window 1)
+    (evil-window-move-very-bottom)
+    (projectile-run-vterm)
+    (evil-insert-state))
 
-(defun leo/vterm-evil-append-line ()
-  (interactive)
-  (execute-kbd-macro (kbd "<end>"))
-  (evil-insert-state))
+  (defun leo/vterm-mode-enter ()
+    (interactive)
+    (setq-local evil-move-cursor-back nil)
+    (rvm-activate-corresponding-ruby))
 
-(defun leo/vterm-evil-insert-line ()
-  (interactive)
-  (execute-kbd-macro (kbd "<home>"))
-  (evil-insert-state))
+  (defun leo/vterm-evil-append-line ()
+    (interactive)
+    (execute-kbd-macro (kbd "<end>"))
+    (evil-insert-state))
+
+  (defun leo/vterm-evil-insert-line ()
+    (interactive)
+    (execute-kbd-macro (kbd "<home>"))
+    (evil-insert-state)))
