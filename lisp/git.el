4873f2605a7401c77435343a2e6eb91ed1530392
Leonardo Santos <aleotory@gmail.com>
2020-07-24 22:51:14 -0300

    Remove unused git functions

--- lisp/git.el
+++ lisp/git.el
@@ -26,32 +26,6 @@
 
 (use-package git-timemachine)
 
-(defun leo/get-current-branch ()
-  (interactive)
-  (message (magit-get-current-branch)))
-
-(defun leo/git-timemachine-show-selected-revision ()
-  "Show last (current) revision of file."
-  (interactive)
-  (let* ((collection (mapcar (lambda (rev)
-                               ;; re-shape list for the ivy-read
-                               (cons (concat (substring-no-properties (nth 0 rev) 0 7) "|" (nth 5 rev) "|" (nth 6 rev)) rev))
-                             (git-timemachine--revisions))))
-    (ivy-read "commits:"
-              collection
-              :action (lambda (rev)
-                        ;; compatible with ivy 9+ and ivy 8
-                        (unless (string-match-p "^[a-z0-9]*$" (car rev))
-                          (setq rev (cdr rev)))
-                        (git-timemachine-show-revision rev)))))
-
-(defun leo/git-timemachine ()
-  "Open git snapshot with the selected version.  Based on ivy-mode."
-  (interactive)
-  (unless (featurep 'git-timemachine)
-    (require 'git-timemachine))
-  (git-timemachine--start #'leo/git-timemachine-show-selected-revision))
-
 (defun leo/with-editor-mode-enter ()
   (interactive)
   (evil-insert-state)
