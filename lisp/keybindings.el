d6d8ab9c147b67af4a31629df0ca6d76e8204caf
Leonardo Santos <aleotory@gmail.com>
2020-07-25 13:20:25 -0300

    Multiple cursors version 0.1.1

--- lisp/keybindings.el
+++ lisp/keybindings.el
@@ -53,12 +53,15 @@
     "<return>" 'newline)
 
   (general-emap
-    "s-a" 'mc/edit-beginnings-of-lines
-    "s-e" 'mc/edit-ends-of-lines
+    "M-a" 'mc/edit-beginnings-of-lines
+    "M-e" 'mc/edit-ends-of-lines
+    "s-<mouse-1>" 'mc/add-cursor-on-click
     "s-d" 'mc/mark-next-like-this)
 
   ;; multiple-cursors-mode
   (general-emap 'mc/keymap
+    "s-f" 'phi-search
+    "s-F" 'phi-search-backward
     "M-1" 'mc/insert-numbers
     "M-A" 'mc/mark-all-dwim
     "M-h" 'mc-hide-unmatched-lines-mode
--- lisp/multiple-cursors.el
+++ lisp/multiple-cursors.el
@@ -1,29 +1,28 @@
-(use-package expand-region
-  :commands (er/expand-region er/mark-word er/mark-symbol))
-
-(use-package evil-mc
+(use-package multiple-cursors
   :after evil
   :config
-  (global-evil-mc-mode t)
-  (setq evil-mc-undo-cursors-on-keyboard-quit t))
-
-(use-package multiple-cursors
-  :config
-  (setq mc/unsupported-minor-modes '(company-mode auto-complete-mode flyspell-mode jedi-mode)
-        mc/always-run-for-all t
-        mc/insert-numbers-default 1)
+  (use-package expand-region :commands (er/expand-region er/mark-word er/mark-symbol))
+  (use-package evil-mc :config (global-evil-mc-mode t))
+  (use-package phi-search)
 
   (add-hook 'multiple-cursors-mode-enabled-hook 'evil-emacs-state)
   (add-hook 'multiple-cursors-mode-disabled-hook 'evil-normal-state)
+
+  (setq mc/unsupported-minor-modes '(company-mode auto-complete-mode flyspell-mode jedi-mode)
+        mc/always-run-for-all t
+        mc/insert-numbers-default 1
+        evil-mc-undo-cursors-on-keyboard-quit t)
+
+
   (defun leo/multiple-cursors-expand-or-mark-next-word ()
     (interactive)
     (if (not (region-active-p))
         (leo/expand-region)
-      (call-interactively #'mc/mark-next-like-this))))
+      (call-interactively #'mc/mark-next-like-this)))
 
-(defun leo/expand-region ()
-  (interactive)
-  (if (evil-emacs-state-p)
-      (call-interactively #'er/expand-region)
-    (evil-emacs-state)
-    (call-interactively #'er/expand-region)))
+  (defun leo/expand-region ()
+    (interactive)
+    (if (evil-emacs-state-p)
+        (call-interactively #'er/expand-region)
+      (evil-emacs-state)
+      (call-interactively #'er/expand-region))))
