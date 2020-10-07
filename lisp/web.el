bc2a9995da515fee82c7f7c9630220d116a64b58
Leonardo Santos <aleotory@gmail.com>
2020-10-07 10:03:39 -0300

    Disable web-mode auto closing tags

--- lisp/web.el
+++ lisp/web.el
@@ -1,10 +1,11 @@
 (use-package web-mode
   :init
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-
+  :config
   (defun leo/web-mode-hook ()
     "Hooks for Web mode."
-    (setq web-mode-markup-indent-offset 2))
+    (setq web-mode-markup-indent-offset 2
+          web-mode-auto-close-style nil))
   (add-hook 'web-mode-hook  'leo/web-mode-hook))
 
 (use-package restclient)
