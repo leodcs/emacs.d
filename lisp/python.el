02386e48621957e54f27ea31f24c8869b6c5cc01
Leonardo Santos <srleodcs@gmail.com>
2021-07-27 12:45:15 -0300

    Install flymake for python

--- lisp/python.el
+++ lisp/python.el
@@ -1,1 +1,11 @@
 (use-package py-autopep8)
+
+(add-hook 'python-mode-hook 'leo/python-mode-enter)
+
+(use-package flymake-python-pyflakes
+  :after flycheck)
+
+(defun leo/python-mode-enter ()
+  (interactive)
+  (flycheck-mode 1)
+  (flymake-python-pyflakes-load))
